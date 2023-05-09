4d697ec2f24a8dc4bf66aae9b605c671b5e0e52c
Shyamanth Kashyap <shyamanth.giggs@gmail.com>
2023-05-09 22:25:19 +0530

    Testcase Dummy

---
+++ src/components/Post.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import Post from './Post';
+
+test('renders the landing page', () => {
+  expect(true).toBe(true);
+});
+
+test('renders the landing page', () => {
+  expect(true).toBe(true);
+});
+
+test('renders the landing page', () => {
+  expect(true).toBe(true);
+});
+
+test('renders the landing page', () => {
+  expect(true).toBe(true);
+});
+
+test('renders the landing page', () => {
+  expect(true).toBe(true);
+});
