2e2e04452d72de7a932de40d7d89fecf92462548
Prajwal-jm25 <prajwal.jm25@gmail.com>
2023-05-09 18:19:29 +0530

    minor fixes

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+// import Post from "./Post";
+import QuestionService from "../service/QuestionService";
+import PostQuestion from "./PostQuestion";
+
+export const Home = () => {
+	const [loading, setLoading] = useState(true);
+    const [questions, setQuestions] = useState();
+
+	useEffect(() => {
+        const fetchData = async ()=>{
+            setLoading(true);
+            try {
+                const response = await QuestionService.listAll();
+                setQuestions(response.data.data);
+            } catch (error) {
+                console.log(error);
+            }
+            setLoading(false);
+        }
+        fetchData();  
+		
+	}, []);
+
+	return (
+		<div >
+			<NavBar key="uniquevalue"/>
+            {/* <PostQuestion></PostQuestion>
+			{!loading && (<Post questions = {questions}/>)} */}
+		</div>
+	);
+};
+
+export default Home;
